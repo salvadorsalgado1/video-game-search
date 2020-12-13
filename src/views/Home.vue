c7904aa501381142b2742be4368aa996aa8049f9
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2020-12-12 22:08:02 -0800

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
             <div class="card-content">
               <p class="heading-description">
                 Search for your favorite titles, ratings, and even go as far as finding information 
-                about your favorite developer and titles. Give it a search to try it out! You can also 
+                about your favorite developers and titles. Give it a search to try it out! You can also 
                 <router-link :to="{name:'SearchByLetter'}">Search By Letter</router-link> if you know the 
                 title of your game.</p>
                <form>
